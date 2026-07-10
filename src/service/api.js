00dc315188c22d3a9138162d626fc5a7f55103e4
Asildar-Hajiyev <tu201806131@code.edu.az>
2026-07-10 22:17:49 +0400

    Update vercel Link

--- src/service/api.js
+++ src/service/api.js
@@ -1,27 +1,40 @@
-import axios from "axios"
+import axios from "axios";
 
-async function getData(){
-    const res = await axios.get('http://localhost:3000/api/data')
-    return res.data
+async function getData() {
+  // const res = await axios.get('http://localhost:3000/api/data')
+  const res = await axios.get(
+    "https://superfon-api-vercel.vercel.app/api/data",
+  );
+  return res.data;
 }
-async function getNavLinks(){
-    const res = await axios.get('http://localhost:3000/api/navlinks')
-    return res.data
+async function getNavLinks() {
+  // const res = await axios.get('http://localhost:3000/api/navlinks')
+  const res = await axios.get(
+    "https://superfon-api-vercel.vercel.app/api/navlinks",
+  );
+  return res.data;
 }
-async function getMenuItems(){
-    const res = await axios.get('http://localhost:3000/api/menuitems')
-    return res.data
+async function getMenuItems() {
+  // const res = await axios.get('http://localhost:3000/api/menuitems')
+  const res = await axios.get(
+    "https://superfon-api-vercel.vercel.app/api/menuitems",
+  );
+  return res.data;
 }
-async function getStoreData(){
-    const res = await axios.get('http://localhost:3000/api/storelocation')
-    return res.data
+async function getStoreData() {
+  // const res = await axios.get('http://localhost:3000/api/storelocation')
+  const res = await axios.get(
+    "https://superfon-api-vercel.vercel.app/api/storelocation",
+  );
+  return res.data;
 }
 
-async function getData3(){
-    const res = await axios.get('http://localhost:3000/api/alldata3')
-    return res.data
+async function getData3() {
+  // const res = await axios.get('http://localhost:3000/api/alldata3')
+  const res = await axios.get(
+    "https://superfon-api-vercel.vercel.app/api/alldata3",
+  );
+  return res.data;
 }
 
-
-
-export {getData,getData3,getNavLinks,getMenuItems,getStoreData }
+export { getData, getData3, getNavLinks, getMenuItems, getStoreData };
